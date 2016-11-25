7e51ce02c30860061ec9eac5146166867d1ce563
toddaaro <social@aarontodd.name>
2016-11-25 10:15:17 -0500

    upgrading workers

--- src/roleAllocator.js
+++ src/roleAllocator.js
@@ -2,14 +2,14 @@ roleAllocator = {
 
     spawnNecessary: function() {
 	    var store = {
-	        'generalist': 6,
+	        'generalist': 8,
 	    };
 
 	    for(role in store) {
 	        var count = store[role];
 	        var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
 	        if(creeps.length < count) {
-		        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: role});
+		        var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: role});
 		        console.log('spawning new ' + role + ': ' + newName);
 	        }
 	    }
