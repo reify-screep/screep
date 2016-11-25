93915c9b4d92f094cb4bb1c636a1b6cd5ac0f764
toddaaro <social@aarontodd.name>
2016-11-25 00:02:53 -0500

    first attempt at resource extraction load balancing

--- src/workersActions.js
+++ src/workersActions.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('workers.actions');
- * mod.thing == 'a thing'; // true
- */
-
 workersActions = {
 
     maybeHarvest: function(creep) {
@@ -17,15 +8,59 @@ workersActions = {
 
     harvest: function(creep) {
         if(creep.memory.state == 'harvesting' && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.targetEnergy = null;
             creep.memory.state = 'deciding';
         }
         if(creep.memory.state == 'harvesting') {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if(!creep.memory.hasOwnProperty('targetEnergy') || creep.memory.targetEnergy == null) {
+                workersActions.selectResource(creep);
+            }
+            var source = creep.memory.targetEnergy;
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
             }
         }
     },
+
+    selectResource: function(creep) {
+
+        // initialize the global registration if it has not been already
+        if !(Memory.hasOwnProperty('energySources')) {
+            Memory.energySources = {};
+            sources = creep.room.find(FIND_SOURCES);
+            for (var i=0; i < sources.length; i++) {
+                Memory.energySources[sources[i]] = [];
+            }
+        }
+
+        // pick by two criteria, the one with least users or in the case of a tie closest
+        var lowestCount = Infinity;
+        var lowestSources = [];
+        for (var source in Memory.energySources) {
+            var currentLength = Memory.energySources[source].length();
+            if(currentLength < lowestCount) {
+                lowestCount = currentLength;
+                lowestSources = [source];
+            } else if(currentLength == lowestCount) {
+                lowestSources.push(source)
+            }
+        }
+
+        // register the creep on the source
+        if(lowestSources.length() == 1) {
+            var source = lowestSources[0];
+            Memory.energySources[source].push(creep.name)
+            creep.memory.targetEnergy = source;
+            return source;
+        } else {
+            // todo : replace this random decision with picking the closest
+            var source = _.shuffle(lowestSources)[0];
+            Memory.energySources[source].push(creep.name)
+            creep.memory.targetEnergy = source;
+            return source;
+        }
+
+    }
 
     upgrade: function(creep) {
         if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
