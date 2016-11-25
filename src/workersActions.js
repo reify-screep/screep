9977f3c4a81d2ae7eca3804e38c4b2d49bd2f357
toddaaro <social@aarontodd.name>
2016-11-25 16:15:01 -0500

    store in nearest storage

--- src/workersActions.js
+++ src/workersActions.js
@@ -142,16 +142,16 @@ workersActions = {
     },
 
     store: function(creep) {
-        var targets = creep.room.find(FIND_STRUCTURES, {
+        var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_EXTENSION ||
                     structure.structureType == STRUCTURE_SPAWN ||
                     structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
             }
         });
-        if(targets.length > 0) {
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0]);
+        if(target != undefined) {
+            if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
             }
         } else {
             creep.memory.state = 'deciding';
