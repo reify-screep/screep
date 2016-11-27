50734438f438b8a5608db4f359bebe5e37a52a21
toddaaro <social@aarontodd.name>
2016-11-27 13:42:31 -0500

    debug

--- src/roleHarvester.js
+++ src/roleHarvester.js
@@ -3,6 +3,7 @@ var _ = require('lodash')
 
 var roleHarvester = {
     run: function(creep) {
+        console.log('creep ' + creep.name + ' about to harvest')
         if(creep.room.name != Memory.home) {
             actions.goHome(creep);
         } else {
