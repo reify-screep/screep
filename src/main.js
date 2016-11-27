f1793ec5acf9975ed94a5acc8a3bf547468ef895
toddaaro <social@aarontodd.name>
2016-11-27 14:21:02 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ var _ = require('lodash')
 
 module.exports.loop = function () {
 
+    // temp fix
+    var ca = Game.creeps['Landon'];
+    delete(ca.memory.assignedResource);
+    delete(ca.memory.assignedStorage);
+    var cb = Game.creeps['Keira'];
+    delete(cb.memory.assignedResource);
+    delete(cb.memory.assignedStorage);
+    delete(Memory.assignments);
+
     Memory.home = 'W8N68';
 
     memoryManager.collect();
