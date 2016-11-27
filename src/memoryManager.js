14259cca4a51a1a4b1363fafbf12176dd47a2142
toddaaro <social@aarontodd.name>
2016-11-27 13:17:05 -0500

    wrong lookup func

--- src/memoryManager.js
+++ src/memoryManager.js
@@ -25,7 +25,7 @@ memoryManager = {
 
     updateStructureStore: function(roomId) {
 
-        var room = Game.getObjectById(roomId);
+        var room = Game.rooms[roomId];
         var structures = room.find(FIND_STRUCTURES);
         var structureMap = {};
 
