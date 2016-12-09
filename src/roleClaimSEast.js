fff2082d2f113ca867fb7a5e199f3c7aea530e15
toddaaro <social@aarontodd.name>
2016-12-09 09:23:14 -0500

    name error

--- src/roleClaimSEast.js
+++ src/roleClaimSEast.js
@@ -4,12 +4,12 @@ var _ = require('lodash');
 var roleClaimSEast = {
     run: function(creep) {
         var targetRoom = "W5N67";
-        log.debug("targeting W5N67");
+        console.log("targeting W5N67");
 
         if(Game.rooms[targetRoom] == undefined) {
             var targetPos = RoomPosition(25, 25, targetRoom);
             res = creep.moveTo(targetPos);
-            log.debug('move to ' + res);
+            console.log('move to ' + res);
             return;
         }
 
